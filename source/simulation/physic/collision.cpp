834efcf56bfbadd13e008efbb6975fa681f647e9
Wachirawich Namgird <hedgaming93@gmail.com>
2024-12-04 20:25:36 +0700

    rework collision detection function to only detect collision

--- source/simulation/physic/collision.cpp
+++ source/simulation/physic/collision.cpp
@@ -23,7 +23,7 @@ glm::vec3 timeToMove3D(glm::vec3 objAPos, glm::vec3 objAVel, glm::vec3 objBPos, 
     std::cout << "      -  Distance and velocity:" << std::endl;
     for (int axis = 0; axis < 3; axis++)
     {
-        std::cout << "           -  Distance: " << objBPos[axis] - objAPos[axis] << ", Total velocity: " << objAVel[axis] - objBVel[axis] << std::endl;
+        std::cout << "           -  Axis: " << axis << ", Distance: " << objBPos[axis] - objAPos[axis] << ", Total velocity: " << objAVel[axis] - objBVel[axis] << std::endl;
 
         // If the time is below zero will mean the object and the target will hit each other in the past in that axis
         float time = timeToMove(objAPos[axis], objBPos[axis], objAVel[axis], objBVel[axis]);
@@ -36,20 +36,13 @@ glm::vec3 timeToMove3D(glm::vec3 objAPos, glm::vec3 objAVel, glm::vec3 objBPos, 
     std::cout << "      -  Negative count: " << negativeCount << std::endl;
     std::cout << "      -  Max negative count: " << maxNegative << std::endl;
     std::cout << "      -  axisTime: " << axisTimes[0] << ", " << axisTimes[1] << ", " << axisTimes[2] << std::endl;
-    if (negativeCount == 0)
+    if (negativeCount == 0 || negativeCount == maxNegative)
     {
         // Object and target will hit each other in the future
         return glm::vec3(axisTimes[0], axisTimes[1], axisTimes[2]);
-        //return pythagoras(glm::vec3(axisTimes[0], axisTimes[1], axisTimes[2]));
-    }
-    else if (negativeCount == maxNegative)
-    {   
-        // Object and target hit each other in the past
-        return glm::vec3(axisTimes[0], axisTimes[1], axisTimes[2]) * -1.0f;
-        //return -pythagoras(glm::vec3(axisTimes[0], axisTimes[1], axisTimes[2]));
     }
     // Object and the target will never hit each other
-    return glm::vec3(0.0f, 0.0f, 0.0f);
+    return glm::vec3(NULL, NULL, NULL);
 }
 
 float collision::dstBaseCD(std::shared_ptr<object::objectBaseClass> object, std::shared_ptr<object::objectBaseClass> target, float deltaTime)
@@ -62,28 +55,56 @@ float collision::dstBaseCD(std::shared_ptr<object::objectBaseClass> object, std:
     glm::vec3 tarScale = target->getScale();
     glm::vec3 tarVel = target->getVelocity();
 
+    std::cout << "      -  Scaling: " << std::endl;
+    std::cout << "           -  Object: " << objScale[0] << ", " << objScale[1] << ", " << objScale[2] << std::endl;
+    std::cout << "           -  Target: " << tarScale[0] << ", " << tarScale[1] << ", " << tarScale[2] << std::endl;
     std::cout << "      -  Origin position: " << std::endl;
-    std::cout << "           -  Object position: " << objPos.x << ", " << objPos.y << ", " << objPos.z << ", target position: " << tarPos.x << ", " << tarPos.y << ", " << tarPos.z << std::endl;
+    std::cout << "           -  Object: " << objPos[0] << ", " << objPos[1] << ", " << objPos[2] << std::endl;
+    std::cout << "           -  Target: " << tarPos[0] << ", " << tarPos[1] << ", " << tarPos[2] << std::endl;
 
-    // Stretch out border of object and target according to whih side of object is correlate to target on all axis
+    // Stretch out border of object and target according to what side of object is correlate to target on all axis
     for (int axis = 0; axis < 3; axis++)
     {
         float side = objPos[axis] - tarPos[axis];
         if (side > 0)
         {
             // Object is on positive side compare to target position
-            objPos[axis] -= objScale[axis] / 2;
             tarPos[axis] += tarScale[axis] / 2;
+
+            // Calculate object border according to if the object is within target border or not
+            side = objPos[axis] - tarPos[axis];
+            if (side > 0)
+            {
+                objPos[axis] -= objScale[axis] / 2;
+            }
+            else if (side < 0)
+            {
+                objPos[axis] += objScale[axis] / 2;
+            }
         }
         else if (side < 0)
         {
             // Object is on negative side compare to target position
-            objPos[axis] += objScale[axis] / 2;
             tarPos[axis] -= tarScale[axis] / 2;
+
+            // Calculate object border according to if the object is within target border or not
+            side = objPos[axis] - tarPos[axis];
+            if (side > 0)
+            {
+                objPos[axis] -= objScale[axis] / 2;
+            }
+            else if (side < 0)
+            {
+                objPos[axis] += objScale[axis] / 2;
+            }
         }
         
     }
 
+    std::cout << "      -  Border position: " << std::endl;
+    std::cout << "           -  Object: " << objPos[0] << ", " << objPos[1] << ", " << objPos[2] << std::endl;
+    std::cout << "           -  Target: " << tarPos[0] << ", " << tarPos[1] << ", " << tarPos[2] << std::endl;
+    
     glm::vec3 travelTimes = timeToMove3D(objPos, objVel, tarPos, tarVel);
     std::cout << "      -  Time comparison: " << std::endl;
     unsigned int collideAxis = 0;
@@ -106,11 +127,11 @@ float collision::dstBaseCD(std::shared_ptr<object::objectBaseClass> object, std:
         std::cout << "                -  contact: " << collideAxis << std::endl;
     }
 
-    float travelTime = pythagoras(travelTimes);
+    //float travelTime = pythagoras(travelTimes);
 
-    std::cout << "      -  Before:" << std::endl;
-    std::cout << "           -  Object border position: " << objPos.x << ", " << objPos.y << ", " << objPos.z << ", target border position: " << tarPos.x << ", " << tarPos.y << ", " << tarPos.z << std::endl;
-    std::cout << "           -  Object velocity: " << objVel.x << ", " << objVel.y << ", " << objVel.z << ", target velocity: " << tarVel.x << ", " << tarVel.y << ", " << tarVel.z << std::endl;
+    //std::cout << "      -  Before:" << std::endl;
+    //std::cout << "           -  Object border position: " << objPos.x << ", " << objPos.y << ", " << objPos.z << ", target border position: " << tarPos.x << ", " << tarPos.y << ", " << tarPos.z << std::endl;
+    //std::cout << "           -  Object velocity: " << objVel.x << ", " << objVel.y << ", " << objVel.z << ", target velocity: " << tarVel.x << ", " << tarVel.y << ", " << tarVel.z << std::endl;
 
     if (collideAxis == 3)
     {
@@ -123,6 +144,7 @@ float collision::dstBaseCD(std::shared_ptr<object::objectBaseClass> object, std:
     
 
     // Object did collide with target
+    /*
     if ((deltaTime > 0.0f && travelTime < deltaTime && travelTime > 0.0f) || (deltaTime < 0.0f && travelTime > deltaTime && travelTime < 0.0f))
     {
         std::cout << "      -  Got collided, Time took: " << std::fixed << std::setprecision(5) << travelTime << ", Delta time: " << deltaTime << std::endl;
@@ -156,5 +178,11 @@ float collision::dstBaseCD(std::shared_ptr<object::objectBaseClass> object, std:
         std::cout << "      -  Not collided, Time took: " << std::fixed << std::setprecision(5) << travelTime << ", Delta time: " << deltaTime << std::endl;
         std::cout << "           -  Object position: " << objPos.x << ", " << objPos.y << ", " << objPos.z << ", target position: " << tarPos.x << ", " << tarPos.y << ", " << tarPos.z << std::endl;
     }
+    */
     return deltaTime;
+}
+
+void collision::collsionResolver(std::shared_ptr<object::objectBaseClass> currentObject, std::shared_ptr<object::objectBaseClass> target, float travelTime)
+{
+
 }
--- source/simulation/physic/collision.h
+++ source/simulation/physic/collision.h
@@ -4,10 +4,13 @@
 #include "fundamental.h"
 #include "momentum.h"
 
+#include <vector>
 #include <memory>
 #include <iomanip>
 
 namespace collision
 {
+    std::vector<float> collisionPairing();
     float dstBaseCD(std::shared_ptr<object::objectBaseClass> currentObject, std::shared_ptr<object::objectBaseClass> target, float deltaTime);
+    void collsionResolver(std::shared_ptr<object::objectBaseClass> currentObject, std::shared_ptr<object::objectBaseClass> target, float travelTime);
 }
